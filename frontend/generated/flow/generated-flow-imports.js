1d4158443291c9714da100324f3d906b905c3e70
Samuel Eickelberg <samuel.eickelberg@live.de>
2023-10-23 10:04:26 +0200

    Updated to Vaadin 24 and Spring Boot PI

---
+++ frontend/generated/flow/generated-flow-imports.js
@@ -0,0 +1,38 @@
+import { injectGlobalCss } from 'Frontend/generated/jar-resources/theme-util.js';
+
+import { css, unsafeCSS, registerStyles } from '@vaadin/vaadin-themable-mixin';
+import $cssFromFile_0 from 'ol/ol.css?inline';
+
+injectGlobalCss($cssFromFile_0.toString(), 'CSSImport end', document);
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/combo-box/theme/lumo/vaadin-combo-box.js';
+import 'Frontend/generated/jar-resources/flow-component-renderer.js';
+import 'Frontend/generated/jar-resources/comboBoxConnector.js';
+import '@vaadin/icons/vaadin-iconset.js';
+import '@vaadin/dialog/theme/lumo/vaadin-dialog.js';
+import '@vaadin/vertical-layout/theme/lumo/vaadin-vertical-layout.js';
+import '@vaadin/tooltip/theme/lumo/vaadin-tooltip.js';
+import '@vaadin/icon/theme/lumo/vaadin-icon.js';
+import '@vaadin/horizontal-layout/theme/lumo/vaadin-horizontal-layout.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import 'Frontend/src/openlayers-connector.js';
+import '@vaadin/common-frontend/ConnectionIndicator.js';
+import '@vaadin/vaadin-lumo-styles/color-global.js';
+import '@vaadin/vaadin-lumo-styles/typography-global.js';
+import '@vaadin/vaadin-lumo-styles/sizing.js';
+import '@vaadin/vaadin-lumo-styles/spacing.js';
+import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/vaadin-iconset.js';
+
+const loadOnDemand = (key) => {
+  const pending = [];
+  if (key === 'd73eacc4168c1f3cd5b2aeb80f229f5ae5a370a5d1d3f89f548b023111ec5435') {
+    pending.push(import('./chunks/chunk-b254a98f5e8a866fd67891b2849cb407d8218cc4430256b39978d69523c2d14e.js'));
+  }
+  return Promise.all(pending);
+}
+
+window.Vaadin = window.Vaadin || {};
+window.Vaadin.Flow = window.Vaadin.Flow || {};
+window.Vaadin.Flow.loadOnDemand = loadOnDemand;
